f36e9a86d1e160a6bd48ff946261cd2c058fe782
Teodor Bozhikov <Teodor.Bozhikov@progress.com>
2017-04-10 18:30:22 +0300

    Optimise publish mechanism

--- compile.sh
+++ compile.sh
@@ -4,6 +4,10 @@ SOURCE_DIR=src;
 PACK_DIR=dist/package/;
 DIST_DIR=dist;
 
+cd $SOURCE_DIR
+npm install 
+cd ..
+
 copy_package_files() {
     cp "$SOURCE_DIR"/package.json $PACK_DIR
     cp "$SOURCE_DIR"/*.md $PACK_DIR
@@ -14,12 +18,10 @@ copy_package_files() {
 #clean dist folder from previous compilation
 rm -rf $DIST_DIR
 
-#install all prerequisites like typescript
-npm install
-
 #create package folder
 mkdir -p $PACK_DIR
 
 #compile package and copy file required by npm
 node_modules/.bin/tsc -p $SOURCE_DIR --outDir $PACK_DIR
+
 copy_package_files
