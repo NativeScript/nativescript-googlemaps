f159d36cf936976f7f50e97907dbe38285b6cde5
vakrilov <alexander.vakrilov@telerik.com>
2017-04-14 11:11:06 +0300

    fix for prototype exception - property should be defined after the class

--- src/nativescript-googlemaps-common.ts
+++ src/nativescript-googlemaps-common.ts
@@ -1,58 +1,25 @@
 import { View, Property } from "tns-core-modules/ui/core/view";
 
-
-// var markerProperty = new Property("marker", "TnsGoogleMaps", new PropertyMetadata(null, PropertyMetadataSettings.None));
-
-// function onMarkerPropertyChanged(data) {
-//     let maps = data.object;
-//     maps._onMarkerPropertyChanged(data);
-// }
-
-// (<PropertyMetadata>markerProperty.metadata).onSetNativeValue = onMarkerPropertyChanged;
-
-export const markerProperty = new Property<TnsGoogleMaps, any>({ name: "marker", defaultValue: null, valueChanged: this._onMarkerPropertyChanged });
-markerProperty.register(TnsGoogleMaps);
-
-function _onMarkerPropertyChanged(target, oldValue, newValue) {
-    if (newValue) {
-        this.addMarker(newValue);
-    } else {
-        this.clearMap();
-    }
-}
-
 export class TnsGoogleMaps extends View {
     marker: any;
 
     public static mapLoadedEvent: string = "mapLoaded";
 
-    private _android: any;
-    private _ios: any;
-    // private __nativeView: any;
-
     public get android(): any {
-        return this._android;
+        return this.nativeView;
     }
 
     public set android(value) {
-        this._android = value;
+        this.nativeView = value;
     }
 
     public get ios(): any {
-        return this._ios;
+        return this.nativeView;
     }
 
     public set ios(value) {
-        this._ios = value;
+        this.nativeView = value;
     }
-
-    // public get _nativeView(): any {
-    //     return this.__nativeView;
-    // }
-
-    // public set _nativeView(value) {
-    //     this.__nativeView = value;
-    // }
 
     public addMarker(marker) {
         //
@@ -62,3 +29,16 @@ export class TnsGoogleMaps extends View {
         //
     }
 }
+
+export const markerProperty = new Property<TnsGoogleMaps, any>({
+    name: "marker",
+    defaultValue: null,
+    valueChanged: (target, oldValue, newValue) => {
+        if (newValue) {
+            target.addMarker(newValue);
+        } else {
+            target.clearMap();
+        }
+    }
+});
+markerProperty.register(TnsGoogleMaps);
